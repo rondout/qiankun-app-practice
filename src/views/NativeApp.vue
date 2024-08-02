6d9f421b54ce1a2076db07dd3126b573c624edd4
shufei.han <shufei.han@gl-inet.com>
2024-08-02 10:58:41 +0800

    feat: 完成部署有关配置

--- src/views/NativeApp.vue
+++ src/views/NativeApp.vue
@@ -2,12 +2,12 @@
  * @Author: shufei.han
  * @Date: 2024-08-01 14:52:30
  * @LastEditors: shufei.han
- * @LastEditTime: 2024-08-01 14:53:28
- * @FilePath: \main-app-vue\src\views\NativeApp.vue
+ * @LastEditTime: 2024-08-01 16:13:13
+ * @FilePath: \qiankun-app-practice\src\views\NativeApp.vue
  * @Description: 
 -->
 <template>
-    This is a Native App
+    This is a Native App v2
 </template> 
 
 <script setup lang="ts">
