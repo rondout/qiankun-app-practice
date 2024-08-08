f39711247a00cd5249e0eba55bf7da7ba3eb709f
shufei.han <shufei.han@gl-inet.com>
2024-08-08 09:12:53 +0800

    chore: ts配置优化

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,12 @@
+/*
+ * @Author: shufei.han
+ * @Date: 2024-08-01 09:38:34
+ * @LastEditors: shufei.han
+ * @LastEditTime: 2024-08-08 09:09:10
+ * @FilePath: \qiankun-app-practice\env.d.ts
+ * @Description: 
+ */
 /// <reference types="vite/client" />
+import 'ant-design-vue/typings/global'
+
+export {}
